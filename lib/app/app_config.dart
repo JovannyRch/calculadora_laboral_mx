85e369e3ef1bfa490af779780afc740d489b256e
jovannyrch <jovannyrch@gmail.com>
2026-06-18 09:55:24 -0600

    feat: update AdMob configuration with correct ad unit IDs and application ID

--- lib/app/app_config.dart
+++ lib/app/app_config.dart
@@ -6,12 +6,12 @@ class AppConfig {
   static const premiumEnabled = false;
   static const isPremiumUser = false;
 
-  static const bannerHomeAdUnitId = 'ca-app-pub-0000000000000000/home-banner';
-  static const bannerHistoryAdUnitId =
-      'ca-app-pub-0000000000000000/history-banner';
-  static const bannerGuideAdUnitId = 'ca-app-pub-0000000000000000/guide-banner';
+  static const androidAdMobAppId = 'ca-app-pub-4665787383933447~6151719227';
+  static const bannerHomeAdUnitId = 'ca-app-pub-4665787383933447/1810088880';
+  static const bannerHistoryAdUnitId = 'ca-app-pub-4665787383933447/8183925542';
+  static const bannerGuideAdUnitId = 'ca-app-pub-4665787383933447/3715227766';
   static const resultInterstitialAdUnitId =
-      'ca-app-pub-0000000000000000/result-interstitial';
+      'ca-app-pub-4665787383933447/9678140505';
 
   static bool get showAds => adsEnabled && !isPremiumUser;
   static bool get showInterstitial => interstitialAdsEnabled && !isPremiumUser;
